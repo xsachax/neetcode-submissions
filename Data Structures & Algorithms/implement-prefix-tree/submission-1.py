a8ffadf4aabc3468db7c9b19ad79f4bc1c4e3026
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-08-19 23:01:50 -0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.isEndOfWord = False
+        self.children = {}
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isEndOfWord = True
+                
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.isEndOfWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+        
+        
